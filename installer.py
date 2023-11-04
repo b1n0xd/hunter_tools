cdc4a1c8c0bd41eed523ddfcb173b4b6bf9c99bb
Luciano Vieira <lucianovieiradev@gmail.com>
2023-11-04 01:08:38 -0300

    feature: insert paramspider tunned.

--- installer.py
+++ installer.py
@@ -3,10 +3,25 @@ from repo import repositoriesGolang
 from repo import repositoriesPython
 
 # Install essentials tools
-os.system("echo 'Instalando ferramentas necessárias...'")
-os.system("apt install golang")
-os.system("apt install golang")
-os.system("pipx install bbot")
+os.system("echo '::Update and Upgrade System ::'")
+os.system("sudo apt-get -y update")
+os.system("sudo apt-get -y upgrade")
+os.system("sudo apt-get install -y git")
+os.system("sudo apt-get install -y xargs")
+os.system("apt install golang -y")
+os.system("apt install python3-full -y")
+os.system("py -3 -m pip install --user pipx")
+os.system("pipx install bbot -y")
+os.system("echo :: Create folders ::")
+os.system("mkdir ~/tools")
+os.system("mkdir ~/tools/zip")
+os.system("mkdir ~/bugbounty")
+os.system("mkdir ~/bugbounty/target")
+os.system("mkdir ~/bugbounty/lists")
+os.system("git clone https://github.com/devanshbatham/paramspider")
+os.system("cd paramspider")
+os.system("pip3 install .")
+os.system("cd ~")
 
 
 # Install tools Golang Based
@@ -18,13 +33,13 @@ for repo in repositoriesPython:
     os.system(f"pip3 install {repo}")
 
 
-os.system("echo 'Installed Tools Golang(base):'")
+os.system("echo 'Installed Tools Golang(based):'")
 for repo in repositoriesGolang:
     os.system('clear')
     tool_name = repo.split("/")[-1]
     os.system(f"echo {tool_name}")
 
-os.system("echo 'Installed tools Python(base):'")
+os.system("echo 'Installed tools Python(based):'")
 for repo in repositoriesPython:
     tool_name = repo.rsplit('/', maxsplit=1)[-1]
     os.system(f"echo {tool_name}")
