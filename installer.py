bf6960dfae7c108654a42e85ec5f87541209f827
Luciano Vieira <lucianovieiradev@gmail.com>
2023-11-04 16:06:47 -0300

    style: update style terminal

--- installer.py
+++ installer.py
@@ -3,7 +3,7 @@ from repo import repositoriesGolang
 from repo import repositoriesPython
 
 # Install essentials tools
-os.system("echo '::Update and Upgrade System::'")
+os.system("echo '[+] Update and Upgrade System [+]:'")
 os.system("sudo apt-get -y update")
 os.system("sudo apt-get -y upgrade")
 os.system("sudo apt-get install -y git")
@@ -12,7 +12,7 @@ os.system("apt install golang -y")
 os.system("apt install python3-full -y")
 os.system("py -3 -m pip install --user pipx")
 os.system("pipx install bbot -y")
-os.system("echo :: Create folders ::")
+os.system("echo [+] Create folders [+]")
 os.system("mkdir ~/tools")
 os.system("mkdir ~/tools/zip")
 os.system("mkdir ~/bugbounty")
@@ -34,12 +34,12 @@ for repo in repositoriesPython:
     os.system(f"pip3 install {repo}")
 
 
-os.system("echo 'Installed Tools Golang(based):'")
+os.system("echo '[+] Installed Tools Golang(based) [+]'")
 for repo in repositoriesGolang:
     tool_name = repo.split("/")[-1]
     os.system(f"echo {tool_name}")
 
-os.system("echo 'Installed tools Python(based):'")
+os.system("echo '[+] Installed tools Python(based) [+]'")
 for repo in repositoriesPython:
     tool_name = repo.rsplit('/', maxsplit=1)[-1]
     os.system(f"echo {tool_name}")
