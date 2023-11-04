0c3d9f607007f901835d9d3524993d4c2ce840d5
Luciano Vieira <lucianovieiradev@gmail.com>
2023-11-04 00:01:19 -0300

    feature: insert bbot install

--- installer.py
+++ installer.py
@@ -2,29 +2,29 @@ import os
 from repo import repositoriesGolang
 from repo import repositoriesPython
 
-# Install Go
+# Install essentials tools
 os.system("echo 'Instalando ferramentas necessárias...'")
 os.system("apt install golang")
 os.system("apt install golang")
 os.system("pipx install bbot")
 
 
-# Install tools Golang
+# Install tools Golang Based
 for repo in repositoriesGolang:
     os.system(f"go install {repo}@latest")
 
-# Install tools Python
+# Install tools Python Based
 for repo in repositoriesPython:
     os.system(f"pip3 install {repo}")
 
 
-os.system("echo 'Installed tools Golang:'")
+os.system("echo 'Installed Tools Golang(base):'")
 for repo in repositoriesGolang:
     os.system('clear')
     tool_name = repo.split("/")[-1]
     os.system(f"echo {tool_name}")
 
-os.system("echo 'Installed tools Python:    '")
+os.system("echo 'Installed tools Python(base):'")
 for repo in repositoriesPython:
     tool_name = repo.rsplit('/', maxsplit=1)[-1]
     os.system(f"echo {tool_name}")
