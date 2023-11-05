de662fc9678772554100d3b14bfd6e6f9616018c
Luciano Vieira <lucianovieiradev@gmail.com>
2023-11-05 02:12:51 -0300

    refactor: insert options to install

--- installer.py
+++ installer.py
@@ -2,32 +2,50 @@ import os
 from repo import repositoriesGolang
 from repo import repositoriesPython
 
-# Install essentials tools
-os.system("echo '[+] Update and Upgrade System [+]:'")
-os.system("sudo apt-get -y update")
-os.system("sudo apt-get -y upgrade")
-os.system("sudo apt-get install -y git")
-os.system("sudo apt-get install -y xargs")
-os.system("apt install golang -y")
-os.system("apt install python3-full -y")
-os.system("py -3 -m pip install --user pipx")
-os.system("pipx install bbot -y")
-os.system("echo [+] Create folders [+]")
-os.system("mkdir ~/tools")
-os.system("mkdir ~/tools/zip")
-os.system("mkdir ~/bugbounty")
-os.system("mkdir ~/bugbounty/target")
-os.system("mkdir ~/bugbounty/lists")
-os.system("git clone https://github.com/devanshbatham/paramspider")
-os.system("cd paramspider")
-os.system("pip3 install .")
-os.system("cd ~")
-os.system("clear")
-os.system("echo '[+] Installing Wordlists at ~/wordlists [+]'  ")
-os.system("git clone https://github.com/trickest/wordlists.git")
-os.system("wget -r --no-parent -R "index.html*" https://wordlists-cdn.assetnote.io/data/ -nH -e robots=of")
-os.system("git clone https://github.com/fuzzdb-project/fuzzdb.git --depth 1")
-os.system("git clone https://gitlab.com/kalilinux/packages/wordlists.git")
+
+optionSelected = int(input("Insert number option:  "))
+
+
+def installer(optionSelected):
+    match optionSelected:
+        case 1:
+            print("[+] Instaling essentials OS... [+]")
+        case 2:
+            print("[+] Instaling Golang Tools Based [+]")
+        case 3:
+            print("[+] Instaling Python Tools Based [+]")
+        case 4:
+            print("[+] Instaling All Worldlists [+]")
+            
+        case _:
+            print("[+] Instaling All Tools Based [+]")
+            os.system("sudo apt-get -y update")
+            os.system("sudo apt-get -y upgrade")
+            os.system("sudo apt-get install -y git")
+            os.system("sudo apt-get install -y xargs")
+            os.system("apt install golang -y")
+            os.system("apt install python3-full -y")
+            os.system("py -3 -m pip install --user pipx")
+            os.system("pipx install bbot -y")
+            os.system("echo [+] Create folders [+]")
+            os.system("mkdir ~/tools")
+            os.system("mkdir ~/tools/zip")
+            os.system("mkdir ~/bugbounty")
+            os.system("mkdir ~/bugbounty/target")
+            os.system("mkdir ~/bugbounty/lists")
+            os.system("git clone https://github.com/devanshbatham/paramspider")
+            os.system("cd paramspider")
+            os.system("pip3 install .")
+            os.system("cd ~")
+            os.system("clear")
+            os.system("echo '[+] Installing Wordlists at ~/wordlists [+]'  ")
+            os.system("mkdir ~/worldlists")
+            os.system("cd ~/worldlists")
+            os.system("git clone https://github.com/trickest/wordlists.git")
+            os.system("wget -r --no-parent -R "index.html*" https://wordlists-cdn.assetnote.io/data/ -nH -e robots=of")
+            os.system("git clone https://github.com/fuzzdb-project/fuzzdb.git --depth 1")
+            os.system("git clone https://gitlab.com/kalilinux/packages/wordlists.git")
+            os.system("cd ~")
 
 
 # Install tools Golang Based
@@ -49,3 +67,6 @@ for repo in repositoriesPython:
     tool_name = repo.rsplit('/', maxsplit=1)[-1]
     os.system(f"echo {tool_name}")
     os.system('sudo mv ~/go/bin/* /usr/bin/ ')
+
+
+installer(optionSelected)
